0f19fc35f55ed82ee33a3036e7572fd367e51a41
mayouli <247067345@qq.com>
2020-12-14 12:02:48 +0800

    暂时先删除一些无用的库

--- app/src/main/java/com/qianfanyun/ycdemo/CoordinatorLayout/nested/CoordinatorNestActivity.kt
+++ app/src/main/java/com/qianfanyun/ycdemo/CoordinatorLayout/nested/CoordinatorNestActivity.kt
@@ -2,12 +2,17 @@ package com.qianfanyun.ycdemo.CoordinatorLayout.nested
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.qianfanyun.ycdemo.R
+import kotlinx.android.synthetic.main.activity_coordinator_nest.*
 
 class CoordinatorNestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coordinator_nest)
+        imv_inner.setOnClickListener {
+            Toast.makeText(this,"inner image click",Toast.LENGTH_SHORT).show()
+        }
     }
 }
